28ff8552fff57e770c835ebe381e5f0752402873
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2021-01-12 19:34:27 +0100

    add method to find latest content file of a spotify URI

--- IO_operations.py
+++ IO_operations.py
@@ -1,7 +1,13 @@
 import json
 import re
 from datetime import date
+import pathlib
+from os import listdir
+import os.path
 
+name_of_content_directory = "content_files"
+
+# ------------------------------------------------ Regular Expression --------------------------------------------------
 # check valid line with a regular expression
 name_re = "[^#].*"  # ".*" is any number (including zero) of any characters (including whitespaces), "^#" matches any
 # character except "#"
@@ -11,6 +17,8 @@ uri_playlist_re = "spotify:playlist:\S*"
 
 def safe_playlist_to_hard_drive(sp, playlist_id):
     """
+    Saves the playlist's content to a file
+
     :param sp: the connection to spotify ( like sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
      for example )
     :param playlist_id: the spotify id of the playlist
@@ -28,7 +36,7 @@ def safe_playlist_to_hard_drive(sp, playlist_id):
         file.close()
 
 
-def read_playlists_and_artists_from_file():
+def read_playlists_and_artists_uris_from_file():
     """
     Reads playlists and artists URIs from file and return them as a list of tuples: (<name>, <URI>)
 
@@ -59,7 +67,7 @@ def read_playlists_and_artists_from_file():
     return p_and_a_list
 
 
-def read_playlists_from_file():
+def read_playlists_uris_from_file():
     """
     Reads playlists URIs from file and return them as a list of tuples: (<name>, <URI>)
 
@@ -89,7 +97,7 @@ def read_playlists_from_file():
     return p_and_a_list
 
 
-def read_artists_from_file():
+def read_artists_uris_from_file():
     """
     Reads artists URIs from file and return them as a list of tuples: (<name>, <URI>)
 
@@ -117,3 +125,46 @@ def read_artists_from_file():
             p_and_a_list.append(tup)
 
     return p_and_a_list
+
+
+def find_latest_content_file(uri):
+    """
+    Searches for the most recently saved content file of the playlist
+
+    :param uri: the spotify URI of the playlist / artist
+    :type uri: str
+    :return: the path as a pathlib.Path object. When no file was found for URI, "None" is returned
+    """
+    # directory structure:
+    # main dir (containing all the .py files)
+    #   - content_files ( =name_of_content_directory )
+    #       -spotify:playlist:<id>
+    #           -spotify:playlist:<id>_content_raw_<date> (dictionary / json file)
+    #           -spotify:playlist:<id>_content_raw_<date2> (dictionary / json file)
+    #           ...
+    #       -spotify:artist:<id>
+    #           ...
+
+    # get main directory (all .py files are in the main directory)
+    main_dir_path = os.path.dirname(__file__)  # returns the directory of this file
+
+    # navigate to the directory containing all content files
+    content_file_dir_path = os.path.join(main_dir_path, name_of_content_directory)
+
+    # list all files and directories of the content directory (NOT recursive)
+    content_overview = listdir(content_file_dir_path)
+
+    for directory in content_overview:
+        if directory == uri and not os.path.isfile(directory):
+            # find the newest / most recently saved content file
+            content_files = listdir(os.path.join(content_file_dir_path, directory))
+            content_files.sort()
+
+            # because of the date format yyyy.mm.dd the newest file will be at the end of the SORTED list
+            latest_content_file = content_files[len(content_files)-1]
+
+            # return the path to the file as a Path object (better than a String when run on different OS)
+            return pathlib.Path(latest_content_file)
+
+    # if no content file is found, return nothing
+    return None
