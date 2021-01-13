b99b536a10484ecea7d9b9a38447b0ce87c39fd4
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2021-01-13 20:02:18 +0100

    refactor print_playlist_changes to Playlist_operations.py

---
+++ Playlist_operations.py
@@ -0,0 +1,150 @@
+import json
+import sys
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from IO_operations import find_latest_content_file
+from IO_operations import read_playlist_and_artists_names_from_file
+from IO_operations import safe_playlist_to_hard_drive
+from URI_operations import get_playlist_id_from_uri
+
+
+def print_playlist_changes(sp, p_uri):
+    """
+    Prints the changes made to the playlist, compared to the latest content file, to the console.
+
+    :param p_uri: the spotify uri of the playlist
+    :param sp: the Spotify API client
+    :type p_uri: str
+    :type sp: spotipy.Spotify
+    """
+    # results is a json file
+    # object{7}:
+    #   href: https://api.spoitfy....
+    #   items [number_of_items]:
+    #       0 {6}:
+    #           added_at: 2020-12-27T23:01:00Z
+    #           added_by {5}:
+    #           is_local : false
+    #           primary_color : null
+    #           track{19}:
+    #               album{13}:
+    #               artists [number_of_artists] :
+    #                   0 {6}:
+    #                       external_urls {1}:
+    #                       href:
+    #                       id:
+    #                       name:
+    #                       type:
+    #                       uri:
+    #                   1 {6}:
+    #                       ...
+    #               available_markets [n_of_avail_markets] :
+    #               disc_number : 1
+    #               duration_ms : 140026
+    #               episode : false
+    #               explicit : true
+    #               external_ids {1}:
+    #               external_urls {1}:
+    #               href: https://api.spotify..
+    #               id : j01u0u401
+    #               is_local : false
+    #               name : Lost (feat. xyz)
+    #               popularity : 80
+    #               preview_url : null
+    #               track : true
+    #               track_number : 1
+    #               type : track
+    #               uri : spotify:track:sjfldlksjl
+    #           video_thumbnail {1} :
+    #       1 {6}:
+    #           .....
+    #
+    #   limit : 100
+    #   next : null
+    #   offset: 0
+    #   previous: null
+    #   total : 50
+
+    # playlist_items yields a dictionary or JSON file
+    results_dict = sp.playlist_items(playlist_id=get_playlist_id_from_uri(p_uri))
+
+    # get the playlists tracks
+    latest_tracks = results_dict["items"]
+
+    # read old playlist content from file
+    latest_content_file = find_latest_content_file(p_uri)
+    print(latest_content_file)
+
+    # if a content file exists for the uri:
+    if latest_content_file:
+        with open(latest_content_file, "r") as oldTrackFile:
+            data = oldTrackFile.readline()  # only reads ONE line (json files have only one (very long) line)
+
+            # if there is another line throw an error
+            if oldTrackFile.readline() != "":
+                print("error: not a json format (file has more than one single line)")
+                # TODO: throw error or stop further execution of this script
+
+            old_results = json.loads(data)
+            old_tracks = old_results["items"]
+
+        # both are a list of dictionaries each containing the song's name and its artists names
+        new_songs = []
+        removed_songs = []
+
+        # print("comparing tracks")
+
+        # check the playlist for new songs
+        for newTrack in latest_tracks:
+            flag_is_new_song = True
+            for oldTrack in old_tracks:
+                # if the current track matches with a track in the old list, then it's not a new song
+                if newTrack["track"]["name"] == oldTrack["track"]["name"]:
+                    flag_is_new_song = False
+                    break
+
+            if flag_is_new_song:
+                song_info = {"name": newTrack["track"]["name"],
+                             "artists": newTrack["track"]["artists"]}
+                new_songs.append(song_info)
+
+        # check the playlist for removed songs
+        for oldTrack in old_tracks:
+            flag_was_removed = True
+            for newTrack in latest_tracks:
+                if oldTrack["track"]["name"] == newTrack["track"]["name"]:
+                    flag_was_removed = False
+                    break
+
+            if flag_was_removed:
+                song_info = {"name": oldTrack["track"]["name"],
+                             "artists": oldTrack["track"]["artists"]}
+                removed_songs.append(song_info)
+
+        print("-------------------------- New Songs: --------------------------")
+        for song in new_songs:
+            artists = ""
+            n_of_artists = len(song["artists"])
+
+            for i in range(0, n_of_artists - 1):
+                artists += song["artists"][i]["name"] + ", "
+            artists += song["artists"][n_of_artists - 1]["name"]
+
+            print(song["name"] + " - " + artists)
+
+        print("------------------------ Removed Songs: ------------------------")
+        for song in removed_songs:
+
+            artists = ""
+            n_of_artists = len(song["artists"])
+
+            for i in range(0, n_of_artists - 1):
+                artists += song["artists"][i]["name"] + ", "
+            artists += song["artists"][n_of_artists - 1]["name"]
+
+            print(song["name"] + " - " + artists)
+    else:
+        print("------------------- No data for playlist yet -------------------")
+
--- spofityPlaylistChecker.py
+++ spofityPlaylistChecker.py
@@ -1,159 +1,54 @@
-import spotipy
-import sys
 import json
+import sys
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+from IO_operations import find_latest_content_file
+from IO_operations import read_playlists_uris_from_file
 from IO_operations import safe_playlist_to_hard_drive
-from IO_operations import read_playlists_from_file
 from URI_operations import get_playlist_id_from_uri
-from spotipy.oauth2 import SpotifyClientCredentials
-from pprint import pprint
+from Playlist_operations import print_playlist_changes
+from Playlist_operations import create_new_private_playlist
 
 
-def print_playlist_changes(p_id):
-    # results is a json file
-    # object{7}:
-    #   href: https://api.spoitfy....
-    #   items [number_of_items]:
-    #       0 {6}:
-    #           added_at: 2020-12-27T23:01:00Z
-    #           added_by {5}:
-    #           is_local : false
-    #           primary_color : null
-    #           track{19}:
-    #               album{13}:
-    #               artists [number_of_artists] :
-    #                   0 {6}:
-    #                       external_urls {1}:
-    #                       href:
-    #                       id:
-    #                       name:
-    #                       type:
-    #                       uri:
-    #                   1 {6}:
-    #                       ...
-    #               available_markets [n_of_avail_markets] :
-    #               disc_number : 1
-    #               duration_ms : 140026
-    #               episode : false
-    #               explicit : true
-    #               external_ids {1}:
-    #               external_urls {1}:
-    #               href: https://api.spotify..
-    #               id : j01u0u401
-    #               is_local : false
-    #               name : Lost (feat. xyz)
-    #               popularity : 80
-    #               preview_url : null
-    #               track : true
-    #               track_number : 1
-    #               type : track
-    #               uri : spotify:track:sjfldlksjl
-    #           video_thumbnail {1} :
-    #       1 {6}:
-    #           .....
-    #
-    #   limit : 100
-    #   next : null
-    #   offset: 0
-    #   previous: null
-    #   total : 50
+REDIRECT_URI = "https://www.duckduckgo.com"
 
-    # playlist_items yields a dictionary or JSON file
-    results_dict = sp.playlist_items(playlist_id=p_id)
-
-    # get the playlists tracks
-    latest_tracks = results_dict["items"]
-
-    # read old playlist content from file
-    with open("ModusMio_content_raw(31.12.2020).txt", "r") as oldTrackFile:
-        data = oldTrackFile.readline()  # only reads ONE line (json files have only one (very long) line)
-
-        # if there is another line throw an error
-        if oldTrackFile.readline() != "":
-            print("error: not a json format (file has more than one single line)")
-            # TODO: throw error or stop further execution of this script
-
-        old_results = json.loads(data)
-        old_tracks = old_results["items"]
-
-    # both are a list of dictionaries each containing the song's name and its artists names
-    new_songs = []
-    removed_songs = []
-
-    # print("comparing tracks")
-
-    # check the playlist for new songs
-    for newTrack in latest_tracks:
-        flag_is_new_song = True
-        for oldTrack in old_tracks:
-            # if the current track matches with a track in the old list, then it's not a new song
-            if newTrack["track"]["name"] == oldTrack["track"]["name"]:
-                flag_is_new_song = False
-                break
-
-        if flag_is_new_song:
-            song_info = {"name": newTrack["track"]["name"],
-                         "artists": newTrack["track"]["artists"]}
-            new_songs.append(song_info)
-
-    # check the playlist for removed songs
-    for oldTrack in old_tracks:
-        flag_was_removed = True
-        for newTrack in latest_tracks:
-            if oldTrack["track"]["name"] == newTrack["track"]["name"]:
-                flag_was_removed = False
-                break
-
-        if flag_was_removed:
-            song_info = {"name": oldTrack["track"]["name"],
-                         "artists": oldTrack["track"]["artists"]}
-            removed_songs.append(song_info)
-
-    print("-------------------------- New Songs: --------------------------")
-    for song in new_songs:
-        artists = ""
-        n_of_artists = len(song["artists"])
-
-        for i in range(0, n_of_artists - 1):
-            artists += song["artists"][i]["name"] + ", "
-        artists += song["artists"][n_of_artists - 1]["name"]
-
-        print(song["name"] + " - " + artists)
-
-    print("------------------------ Removed Songs: ------------------------")
-    for song in removed_songs:
-
-        artists = ""
-        n_of_artists = len(song["artists"])
-
-        for i in range(0, n_of_artists - 1):
-            artists += song["artists"][i]["name"] + ", "
-        artists += song["artists"][n_of_artists - 1]["name"]
-
-        print(song["name"] + " - " + artists)
-
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-
-playlist_list = read_playlists_from_file()
-
-# Check if the save-to-hard-drive-flag has been set
+# Check if any of the flags have been set
 flag_save_content_to_file = False
+flag_authorization_code_flow = False
 if len(sys.argv) > 1:
     print(sys.argv)
-    if sys.argv[1] == "save":
-        flag_save_content_to_file = True
+    for arg in sys.argv:
+        if arg == "save":
+            flag_save_content_to_file = True
 
-# playlist_data is a tuple (name, URI)
-for playlist_data in playlist_list:
-    name = playlist_data[0]
-    playlist_id = get_playlist_id_from_uri(playlist_data[1])
+        if arg == "authorization":
+            flag_authorization_code_flow = True
 
-    print("\n\n##########################################")
-    print("Checking content of '" + name + "'")
-    print("Playlist ID: " + playlist_id)
-    print("##########################################\n\n")
-    print_playlist_changes(playlist_id)
 
-    if flag_save_content_to_file:
-        print("############ Saving Content of " + name + " To Hard Drive ############")
-        safe_playlist_to_hard_drive(sp, playlist_id)
+if flag_authorization_code_flow:
+    scope = "playlist-modify-private"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, redirect_uri=REDIRECT_URI))
+
+    create_new_private_playlist(sp, "generated With Script")
+else:
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
+    playlist_list = read_playlists_uris_from_file()
+
+    # playlist_data is a tuple (name, URI)
+    for playlist_data in playlist_list:
+        name = playlist_data[0]
+        uri = playlist_data[1]
+        playlist_id = get_playlist_id_from_uri(uri)
+
+        print("\n\n##########################################")
+        print("Checking content of '" + name + "'")
+        print("Playlist ID: " + playlist_id)
+        print("##########################################\n")
+        print_playlist_changes(sp, uri)
+
+        if flag_save_content_to_file:
+            print("############ Saving Content of " + name + " To Hard Drive ############")
+            safe_playlist_to_hard_drive(sp, uri)
